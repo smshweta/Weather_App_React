b1d057b8c9ee56bed7b897c3b9200ac0c3bb7a30
mshweta <mshweta@bu.edu>
2023-02-11 13:49:53 -0500

    add_initial_files

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': 'ddbace9ecemsh817470ba69214f6p1141bbjsn52a415aa9d4e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "162ac2ccdaf049bb8b118b42c0d42dcf"; 
